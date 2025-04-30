4c1c5598725a2dfadcd88a1ada4c981e6caa0e7a
Divine <odeydivine16@gmail.com>
2025-04-30 13:44:12 +0100

    i adjuested the icon with JS

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menu = document.querySelector("#menu")
 const nav = document.querySelector("ul")
+const closed = document.querySelector("#closed")
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("visible")
 })
+closed.addEventListener("click", () => {
+    nav.classList.remove("visible")
+})
+
+// const back = document.querySelector("body")
+// const colour = prompt("Enter a colour for the background")
+// back.style.color = colour
